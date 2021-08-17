883415d4d26deccca3c0c488442a12cc915f045d
Enchan <enchant0130@gmail.com>
2021-08-17 10:52:28 +0900

    Initial Commit

---
+++ ExternalProcessEx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ExternalProcessEx
+//
+//  Created by EnchantCode on 2021/08/17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
